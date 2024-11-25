d45c5b71f4002aa041d2799e22937d3e41f028a3
The007who <awsomeg75@gmail.com>
2024-11-25 22:43:18 +0000

    added links to footer

--- docs/js/script.js
+++ docs/js/script.js
@@ -8,11 +8,12 @@ function startUp() {
         const commits = data.map(commit => {
             const date = commit.commit.author.date
             const message = commit.commit.message
+            const link = commit.html_url
 
             const new_date = date.substring(8, 10) + months[date.substring(5, 7) - 1]
-                + date.substring(2, 4)
+                + date.substring(0, 4)
 
-            return `<li>${new_date}: ${message}</li>`;
+            return `<li><a href="${link}">${new_date}: ${message}</a></li>`;
         });
         document.getElementById('commit-history').innerHTML = commits.join('');
     });
