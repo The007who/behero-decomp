6d7caa929761624932f759c06ce0a46bdd83fb25
The007who <awsomeg75@gmail.com>
2024-11-29 11:55:28 +0000

    new cards and news footer fix

--- docs/js/script.js
+++ docs/js/script.js
@@ -3,34 +3,42 @@ months = [
 ]
 
 function startUp() {
+    let msgLength = 0;
     fetch('https://api.github.com/repos/The007who/behero-decomp/commits?sha=gh-pages').then(response => response.json())
     .then(data => {
         const commits = data.map(commit => {
-            const date = commit.commit.author.date
-            const message = commit.commit.message
-            const link = commit.html_url
+            const date = commit.commit.author.date;
+            const message = commit.commit.message;
+            const link = commit.html_url;
 
             const new_date = date.substring(8, 10) + months[date.substring(5, 7) - 1]
-                + date.substring(0, 4)
+                + date.substring(0, 4);
 
-            return `<li><a href="${link}">${new_date}: ${message}</a></li>`;
+            msgLength += new_date.length + message.length + 2;
+            if (msgLength < 250) {
+                return `<li><a href="${link}">${new_date}: ${message}</a></li>`;
+            } else {
+                return '';
+            }
         });
+
         document.getElementById('commit-history').innerHTML = commits.join('');
     });
 }
 
 
-document.getElementsByTagName("div div img").onclick = function imageClick () {
-    console.log("clicked")
-}
-
 
 function imageClick2 () {
-    const image = document.getElementsByTagName();
+    const image = document.getElementsByClassName('content');
 
     image.addEventListener('click', () => {
-    const currentScale = getComputedStyle(image).getPropertyValue('transform').split(' ')[0].replace('scale(', '').replace(')', '');
-    const newScale = parseFloat(currentScale) + 0.5;
-    image.style.transform = `scale(${newScale})`;}
-    );
+        const currentScale = getComputedStyle(image)
+            .getPropertyValue('transform')
+            .split(' ')[0]
+            .replace('scale(', '')
+            .replace(')', '');
+
+        const newScale = parseFloat(currentScale) + 0.5;
+        image.style.transform = `scale(${newScale})`;
+    });
 }
