61401d270efeaf9ecc278a497d1f8bf1cc67ed7f
The007who <awsomeg75@gmail.com>
2025-10-25 01:17:24 +0100

    better stuff

---
+++ firm-analysis/merge-firm.py
@@ -0,0 +1,32 @@
+import filecmp
+from os import listdir
+from os.path import isfile, dirname, join
+
+script_dir = dirname(__file__)
+
+files = []
+for item in listdir(join(script_dir, 'files')):
+    files.append(join(script_dir, 'files', item))
+
+filedata = [open(filename, 'rb') for filename in files]
+
+with open(join(script_dir, 'firm.bin'), 'wb') as output_file:
+    with open(join(script_dir, 'merge.csv'), 'w') as csv_file:
+        for pos in range(0x4000000):
+            values = {}
+            for file in filedata:
+                buffer = file.read(1)
+                if buffer in values:
+                    values[buffer] += 1
+                else:
+                    values[buffer] = 0
+            
+            highest_key = max(values, key=values.get)
+            highest_value = values[highest_key]
+            
+            output_file.write(highest_key)
+            csv_file.write(str(highest_value) + '\n')
+
+            print(pos / 0x4000000 * 100)
+
+
--- py/README.md
+++
@@ -1,2 +0,0 @@
-# Automated Serial Commands
-To automate the command to read the EEPROM contents I am going to [script](https://github.com/digiampietro/hacking-gemtek/blob/master/serial-flash-dump.expect)
