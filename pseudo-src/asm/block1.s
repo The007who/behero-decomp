84a744f7d28cebfb3c6fc5493fe0ff5f05db1e50
The007who <awsomeg75@gmail.com>
2026-01-28 04:00:39 +0000

    added more assembly instructions

--- pseudo-src/asm/block1.s
+++ pseudo-src/asm/block1.s
@@ -75,14 +75,14 @@
 	r1 = $5011		; (9309 5011)
 	[$783a] = r1	; (d319 783a)
 
-	; maybe lcd setup
+	; lcd setup
 	r1 = $2492		; (9309 2492)
 	[$7874] = r1	; (d319 7874)
 	[$787c] = r1	; (d319 787c)
 	[$7888] = r1	; (d319 7888)
 	[$787e] = r1	; (d319 787e)
 
-	; maybe test mode, $7860 might be a hidden input port 
+	; maybe test mode, $7860 might be a hidden port
 	r1 = [$7860]	; (9311 7860)
 	test r1, $0080	; (c309 0080)
 	je exit			; (5e04)
@@ -94,8 +94,6 @@
 	
 	;
 	sp -= $04		; (2044)
-
-	;
 	r2 = sp + $0001	; (0508 0001)
 	r1 = $00		; (9240)
 	[r2++] = r1		; (d2d2)
@@ -105,3 +103,24 @@
 	[r2++] = r1		; (d2d2)
 	[r2] = r3		; (d6c2)
 	call $0009bc	; (f040 09bc)
+
+	;
+	sp += $04		; (0044)
+	ds = $00		; (fe00)
+	r1 = $048c		; (9309 048c)
+	r2 = $2fea		; (950a 2fea)
+	r3 = $0016		; (970b 0016)
+	loop:
+	r4 = ds:[r1++]	; (98f1)
+	[r2++] = r4		; (d8d2)
+	r3 -= $01		; (2641)
+	jne loop		; (4e44)
+	call $000aeb	; (f040 0aeb)
+	infinite:
+	jmp infinite	; (4e44)
+
+	nop				; (f165)
+	nop				; (f165)
+	nop				; (f165)
+	nop				; (f165)
+	reti			; (9a98)
