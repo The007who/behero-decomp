ce1bae3efffa1f0d89c5c43bc1bfc76d17006197
The007who <awsomeg75@gmail.com>
2026-09-16 14:57:26 +0100

    new instructions

--- pseudo-src/asm/09bc.s
+++ pseudo-src/asm/09bc.s
@@ -77,15 +77,125 @@ r4 = 01
 r4 = [bp+0d]
 [bp+0b] = r4
 
-r4 = 
+r4 = [bp+0b]
+[bp+0a] = r4
 
+r4 = [bp+0a]
+[bp+08] = r4
 
+r4 = [bp+08]
+[bp+06] = r4
 
+r4 = [bp+06]
+[bp+05] = r4
 
+r4 = [bp+05]
+[bp+04] = r4
 
+r4 = 01
+[bp+11] = r4
 
+r2 = 0
+r3 = [bp+00]
 
+r4 = r3 asr 4
+r4 = r4 asr 4
+r4 = r4 asr 4
+r4 = r4 asr 4
 
+r3 += [bp+15]
+r4 += [bp+16], carry
 
+ds = r4
+ds:[r3] = r2
+r4 = 0
+[bp+02] = r4
 
+big_back_jump:
+r3 = [bp+02]
+r4 = [bp+00]
+cmp r3, r4
+jl skip
+goto big_forward_jump
 
+skip:
+r2 = [bp+11]
+r3 = [bp+02]
+r4 = r3 asr 4
+r4 = r4 asr 4
+r4 = r4 asr 4
+r4 = r4 asr 4
+
+r3 += [bp+15]
+r4 += [bp+16], carry
+ds = r4
+ds:[r3] = r2
+
+r2 = [bp+02]
+r3 = [bp+02]
+r4 = r3 asr 4
+r4 = r4 asr 4
+r4 = r4 asr 4
+r4 = r4 asr 4
+
+r3 += [bp+15]
+r4 += [bp+16], carry
+ds = r4
+r3 = ds:[r3]
+r4 = r3 asr 4
+r4 = r4 asr 4
+r4 = r4 asr 4
+r4 = r4 asr 4
+
+r3 += [bp+17]
+r4 += [bp+18], carry
+ds = r4
+ds:[r3] = r2
+r4 = [bp+02]
+r3 = r4 asr 4
+r3 = r3 asr 4
+r3 = r3 asr 4
+r3 = r3 asr 4
+
+r1 = bp + 03
+r2 = 0
+r4 += r1
+r3 += r2, carry
+ds = r3
+r4 = ds:[r4]
+cmp r4, 0
+je 0a55
+
+r3 = [bp + 00]
+r4 = r3 asr 4
+r4 = r4 asr 4
+r4 = r4 asr 4
+r4 = r4 asr 4
+
+r3 += [bp+15]
+r4 += [bp+16], carry
+ds = r4
+r4 = ds:[r3]
+r4 ^= [bp+11]
+r2 = [bp+00]
+
+r3 = r2 asr 4
+r3 = r3 asr 4
+r3 = r3 asr 4
+r3 = r3 asr 4
+
+r2 += [bp+15]
+r3 += [bp+16], carry
+ds = r3
+ds:[r2] = r4
+
+0a55:
+r4 = [bp+11]
+r4 = r4 lsl 1
+[bp+11] = r4
+r4 = [bp+02]
+r4 += 01
+[bp+02] = r4
+goto big_back_jump
+
+big_forward_jump:
